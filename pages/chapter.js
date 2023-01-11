7b28916ca38cbcf98dc310d528a70a42a2997bc7
rol <none@none.com>
2023-01-11 05:45:56 +0000

    chapter 4.1 cpl

---
+++ pages/chapter.js
@@ -0,0 +1,11 @@
+import Hero from '../components/hero'
+
+const Chapter = () => {
+    return (
+        <Hero
+         title="Chapter" 
+         subtitle="4.1"
+         />
+    )
+}
+export default Chapter
