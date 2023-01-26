22fb6da0a49a651dcbc152370ef855eb7b490882
itc-s22020 <s22020@std.it-college.ac.jp>
2023-01-26 06:47:47 +0000

    chapter 7.3 e

---
+++ pages/blog/schedule.js
@@ -0,0 +1,15 @@
+const Schedule = () => {
+    return (
+        <h1>記事のタイトル</h1>
+    )
+}
+
+const getStaticProps = async () => {
+    console.log('処理1')
+    console.log('処理2')
+    console.log('処理3')
+
+    return {
+        props: {},
+    }
+}
