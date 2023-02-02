7267a39ca148313df0c078970b65b1132347ffd9
itc-s22020 <s22020@std.it-college.ac.jp>
2023-02-02 10:14:40 +0000

    chapter 8.3 cpl

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,16 +1,48 @@
 import { getPostBySlug } from 'lib/api'
 import Container from '@/components/container'
+import PostHeader from '@/components/post-header'
+import Image from 'next/image'
+import { height, width } from '@fortawesome/free-brands-svg-icons/faTwitter'
+import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from '@/components/two-column'
+import PostBody from '@/components/post-body'
 
 export default function Schedule({
     title, 
     publish,
-    contect,
-    eyecathc,
+    content,
+    eyecatch,
     categories,
 }) {
     return (
         <Container>
-            <h1>{title}</h1>
+            <article>
+                <PostHeader title={title} subtitle="Blog Article" publish={publish}/>
+
+                <figure>
+                    <Image 
+                        src={eyecatch.url}
+                        alt=""
+                        style={{
+                            width: '100%',
+                            height: 'auto'
+                        }}
+                        width={eyecatch.width}
+                        height={eyecatch.height}
+                        sizes="(min-width: 1152px) 1152px, 100vw"
+                        priority
+                    />
+                </figure>
+
+                <TwoColumn>
+                    <TwoColumnMain>
+                        <PostBody>
+                            <div dangerouslySetInnerHTML={{ __html: content}} />
+                        </PostBody>
+                    </TwoColumnMain>
+                    <TwoColumnSidebar></TwoColumnSidebar>
+                </TwoColumn>
+
+            </article>
         </Container>
     )
 }
@@ -23,8 +55,8 @@ export async function getStaticProps() {
         props: {
             title: post.title,
             publish: post.publishDate,
-            contect: post.content,
-            eyecathc: post.eyecatch,
+            content: post.content,
+            eyecatch: post.eyecatch,
             categories: post.categories,
         },
     }
