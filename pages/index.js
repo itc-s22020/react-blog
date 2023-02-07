82eb4e716e1885d8c3ed06695d06dc88278b0b77
itc-s22020 <s22020@std.it-college.ac.jp>
2023-02-07 10:48:41 +0000

    Chapter 9.9 cpl

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
+import { getAllPosts } from 'lib/api'
 //import Hero from '@/components/hero'
 import Meta from '@/components/meta'
 import Container from '@/components/container'
 import Hero from '../components/hero'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
+import Posts from '@/components/posts'
+import Pagination from '@/components/pagination'
 
-const Home = () => {
+export default function Home({ posts }) {
      return (  
           <Container>
                <Meta />
@@ -11,8 +16,26 @@ const Home = () => {
                     title="CUBE" 
                     subtitle="アウトプットしていくサイト"
                     imageOn
-               />
+               /> 
+               <Posts posts={posts} />
+               <Pagination nextUrl='/blog' nextText='More Posts' />
           </Container>
      )
 }
-export default Home
+export async function getStaticProps() {
+     const posts = await getAllPosts(4)
+
+     for (const post of posts) {
+          if (!post.hasOwnProperty('eyecatch')) {
+               post.eyecatch = eyecatchLocal
+          }
+          const { base64 } = await getPlaiceholder(post.eyecatch.url)
+          post.eyecatch.blurDataURL = base64
+     }
+
+     return {
+          props: {
+               posts: posts,
+          },
+     }
+}
