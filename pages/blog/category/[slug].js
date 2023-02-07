5151aae0bff60131e954f1711c4447045dfcf9e7
itc-s22020 <s22020@std.it-college.ac.jp>
2023-02-07 11:03:38 +0000

    Chapter 9 all cpl

--- pages/blog/category/[slug].js
+++ pages/blog/category/[slug].js
@@ -1,11 +1,17 @@
-import { getAllCategories } from "lib/api";
+import { getAllCategories, getAllPostsByCategories } from "lib/api";
 import Container from "@/components/container";
 import PostHeader from "@/components/post-header";
+import Posts from "@/components/posts";
+import { getPlaiceholder } from "plaiceholder";
+import { eyecatchLocal } from "lib/constants";
+import Meta from "@/components/meta";
 
-export default function Category({ name }) {
+export default function Category({ name, posts }) {
     return (
         <Container>
+            <Meta pagetitle={name} pageDesc={`${name}に関する記事`} />
             <PostHeader title={name} subtitle="Blog Category" />
+            <Posts posts={posts} />
         </Container>
     )
 }
@@ -24,9 +30,19 @@ export async function getStaticProps(context) {
     const allCats = await getAllCategories()
     const cat = allCats.find(({ slug }) => slug === catSlug )
 
+    const posts = await getAllPostsByCategories(cat.id)
+
+    for (const post of posts) {
+        if (!post.hasOwnProperty('eyecatch')) {
+            post.eyecatch = eyecatchLocal
+        }
+        const { base64 } = await getPlaiceholder(post.eyecatch.url)
+        post.eyecatch.blurDataURL = base64
+    }
     return {
         props: {
             name: cat.name,
+            posts: posts,
         }, 
     }
 }
